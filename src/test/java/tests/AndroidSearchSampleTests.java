2857e1a3a0188570337fb05db5da2fc479fd261c
Oleg_Zaharenko <zakharenka.oleg@gmail.com>
2022-08-17 18:25:11 +0300

    a

--- src/test/java/tests/AndroidSearchSampleTests.java
+++ src/test/java/tests/AndroidSearchSampleTests.java
@@ -4,7 +4,9 @@ import com.codeborne.selenide.CollectionCondition;
 import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
@@ -13,11 +15,26 @@ public class AndroidSearchSampleTests {
     void searchTest() {
         step("Type search", () -> {
             $(AppiumBy.accessibilityId("Search Wikipedia")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text"))
-                    .sendKeys("BrowserStack");
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("BrowserStack");
         });
         step("Verify content found", () ->
                 $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
                         .shouldHave(sizeGreaterThan(0)));
+    }
+
+    @Test
+    void searchTest2() {
+        step("Type search", () -> {
+            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Wikipedia");
+        });
+        step("select an element from the list", () -> {
+            $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).findBy(text("Wikimedia Commons")).click();
+
+        });
+        step("Check the opened page", () -> {
+            $(AppiumBy.id("pcs-edit-section-title-description")).shouldHave(text
+                    ("Online media repository of free-use images, sounds and other media files"));
+        });
     }
 }
