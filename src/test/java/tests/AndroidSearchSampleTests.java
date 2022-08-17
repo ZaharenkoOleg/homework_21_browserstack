ff92447d6781be2d82eb45bf4ae9e402214579f4
Oleg_Zaharenko <zakharenka.oleg@gmail.com>
2022-08-17 19:23:06 +0300

    a

--- src/test/java/tests/AndroidSearchSampleTests.java
+++ src/test/java/tests/AndroidSearchSampleTests.java
@@ -1,8 +1,6 @@
 package tests;
 
-import com.codeborne.selenide.CollectionCondition;
 import io.appium.java_client.AppiumBy;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
@@ -10,7 +8,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
-public class AndroidSearchSampleTests {
+public class AndroidSearchSampleTests extends TestBase {
     @Test
     void searchTest() {
         step("Type search", () -> {
@@ -31,10 +29,6 @@ public class AndroidSearchSampleTests {
         step("select an element from the list", () -> {
             $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).findBy(text("Wikimedia Commons")).click();
 
-        });
-        step("Check the opened page", () -> {
-            $(AppiumBy.id("pcs-edit-section-title-description")).shouldHave(text
-                    ("Online media repository of free-use images, sounds and other media files"));
         });
     }
 }
